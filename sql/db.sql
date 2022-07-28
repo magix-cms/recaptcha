738cb4f98a3bb9723a2a9df74d6c43b6e9a6433a
Aurelien <gerits.aurelien@gmail.com>
2022-07-28 08:44:25 +0200

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -7,6 +7,3 @@ CREATE TABLE IF NOT EXISTS `mc_recaptcha` (
   `date_register` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_recaptcha`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-
-INSERT INTO `mc_admin_access` (`id_role`, `id_module`, `view`, `append`, `edit`, `del`, `action`)
-  SELECT 1, m.id_module, 1, 1, 1, 1, 1 FROM mc_module as m WHERE name = 'recaptcha';
